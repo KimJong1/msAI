e81c6b937290ab023cf9194c5a29454fbc6dbfdb
KimJong1 <kjw5859@gmail.com>
2025-07-09 15:18:39 +0900

    변경

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,5 @@ pip install openai
 pip install os
 pip install load_dotenv
 pip install pandas
-pip install BytesIO
-pip install re
 
 python -m streamlit run aiSearch.py --server.port 8000 --server.address 0.0.0.0
