59a64471c7ac039a56e81d6f45f6eb1bc789791c
KimJong1 <95452684+KimJong1@users.noreply.github.com>
2025-07-09 17:34:20 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,6 @@ pip install streamlit
 pip install openai
 pip install load_dotenv
 pip install pandas
+pip install xlsxwriter
 
 python -m streamlit run aiSearch.py --server.port 8000 --server.address 0.0.0.0
