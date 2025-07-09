35321eb1a13d9e206fd656007c8dd6c16a44ae7d
KimJong1 <95452684+KimJong1@users.noreply.github.com>
2025-07-09 17:41:16 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,6 @@ pip install load_dotenv
 pip install pandas
 pip install xlsxwriter
 pip install openpyxl
+pip install tabulate
 
 python -m streamlit run aiSearch.py --server.port 8000 --server.address 0.0.0.0
